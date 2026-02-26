4c24b4860fbe35c25823e84057779a82b8319cca
ARISH570 <ARISH570@users.noreply.github.com>
2026-02-26 11:59:51 +0530

    Fix Express 5 CORS crash

--- server.js
+++ server.js
@@ -53,8 +53,15 @@ app.use(cors({
     credentials: true
 }));
 
-app.options("/*", (req, res) => {
-    res.sendStatus(204);
+app.use((req, res, next) => {
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+        res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.header("Access-Control-Allow-Credentials", "true");
+        return res.sendStatus(204);
+    }
+    next();
 });
 
 // Routes
